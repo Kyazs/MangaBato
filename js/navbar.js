c32243d3ac910afab64ed6bbdadaaa106f734304
Kyazs <santosjohncasper@gmail.com>
2024-04-01 19:54:23 +0800

    added clamp.js library for ellipsis

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,19 @@
-function showSidebar(){
+function showSidebar() {
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'flex'
 }
 
 function closeSidebar() {
     const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display='none'
+    sidebar.style.display = 'none'
+}
+
+// ! a function to add ellipsis to the text when it overflows  by using clamp.js library
+window.onload = function () {
+    // ! ellipsis will be added when it overflow in line 2
+    var element = document.getElementById('getClap2');
+    $clamp(getClap2, { clamp: 2 });;
+    // ! ellipsis will be added when it overflow in line 1
+    var element = document.getElementById('getClap1');
+    $clamp(getClap1, { clamp: 1 });;
 }
