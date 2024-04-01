3d95524378f522ad5b7544d71604c09559a45012
Kyazs <santosjohncasper@gmail.com>
2024-04-01 23:10:54 +0800

    made a seperate file for featured

---
+++ assets/php/trending.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/trending.css">
+</head>
+
+<body>
+    <section class="section-container-trending">
+        <div class="trending-container">
+            <div class="topbar">
+                <p>Featured</p>
+            </div>
+            <header class="trending">
+                <div class="box">
+                    <a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                        <p>BERSERK</p>
+                    </a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                        <p>BERSERK</p>
+                    </a>
+                </div>
+                <div class="box">
+                    <a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                        <p>BERSERK</p>
+                    </a>
+                </div>
+                <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+            <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+            <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+            <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+            <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+            <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+            <div class="box"><a href="#"><img src="../../img/books/berserk.jpg" alt="Berserk">
+                    <p>BERSERK</p>
+                </a></div>
+        
+            </header>
+        </div>
+    </section>
+</body>
+
+</html>
