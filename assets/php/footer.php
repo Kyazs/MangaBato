3f6e01e6b22df2118bcf53c9e94a9b8e50cb9723
Kyazs <santosjohncasper@gmail.com>
2024-04-02 22:55:10 +0800

    added footer on mangapage footer

--- assets/php/footer.php
+++ assets/php/footer.php
@@ -5,18 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MANGABATO</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+
 </head>
 
 <body>
     <!-- Your footer content here -->
     <footer>
-        <ul>
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">Terms of Service</a></li>
-            <li><a href="#">About us</a></li>
-        </ul>
-        <p>C 2024 - Present MangaBato</p>
+        <div class="footer-container">
+            <!-- ! ICONS FROM BOXICONS -->
+            <div class="icons">
+                <a href="https://www.facebook.com">
+                    <i class='bx bxl-facebook-circle'></i></a>
+                <a href="https://www.discord.com">
+                    <i class='bx bxl-discord-alt'></i></a>
+                <a href="https://www.reddit.com">
+                    <i class='bx bxl-reddit'></i></a>
+                <a href="https://github.com/Kyazs/MangaBato">
+                    <i class='bx bxl-github'></i></a>
+            </div>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">Terms of Service</a></li>
+                <li><a href="#">About us</a></li>
+            </ul>
+            <p>© 2024 - Present MangaBato</p>
+        </div>
     </footer>
     <script src="js/navbar.js"></script>
 </body>
