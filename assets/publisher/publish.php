6f4e3a29b9f8ba2a38921a92ff53d2994846c824
Kyazs <santosjohncasper@gmail.com>
2024-04-11 17:59:49 +0800

    publish created

---
+++ assets/publisher/publish.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>mangabato</title>
+
+    <link rel="stylesheet" href="publish.css">
+    <!-- link for the star icon -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
+    <!-- link for the boxicons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <nav> <?php include('../php/navbar.php'); ?> </nav>
+
+    <main>
+        <nav>
+
+            <ul>
+                <li><a href="../../index.html">
+                        < Home</a>
+                </li>
+                <li><a href="../../publish.php">
+                        < Publish</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="publish-container">
+            <header>
+                <div class="head">
+                    <h2>MANGA</h2>
+                    <h2>CREATED ON</h2>
+                    <h2>LAST UPDATE</h2>
+                </div>
+                <!--👌 MAIN CONTAINER FOR THE CONTENTS -->
+                <article>
+                    <!-- MANGA CREATED 😵‍💫 -->
+                    <div class="manga">
+                        <div class="box-container">
+                            <div class="box">
+                                <div class="rank_image"><a href="../../img/books/berserk.jpg "><img src="../../img/books/berserk.jpg" alt=""></a>
+                                </div>
+                                <div class="chapter_contents">
+                                    <a class="Tfont" class="title" href="../mangapage/mangapage.php">BERSERK ajksd asdkj akjd</a>
+                                    <ul>
+                                        <!--  GENRES-->
+                                        <li>Genre:</li>
+                                        <li>Adventure, </li>
+                                        <li>Fantasy, </li>
+                                        <li>Horror, </li>
+                                        <li>Mature, </li>
+                                        <li>Seinen, </li>
+                                        <li>Supernatural, </li>
+                                        <li>Tragedy, </li>
+                                    </ul>
+                                    <div class="star-rating">
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">star_half</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- 😒 -->
+                            <div class="created-on">
+                                <p>4/11/2024</p>
+                            </div>
+                            <div class="last-update">
+                                <p>3/21/2024</p>
+                            </div>
+                        </div>
+
+                        <div class="box-container">
+                            <div class="box">
+                                <div class="rank_image"><a href="../../img/books/berserk.jpg "><img src="../../img/books/gate.jpg" alt=""></a>
+                                </div>
+                                <div class="chapter_contents">
+                                    <a class="Tfont" class="title" href="../mangapage/mangapag-gate.php">Gate - Jietai Kare No Chi Nite, Kaku Tatakeri</a>
+                                    <ul>
+                                        <!--  GENRES-->
+                                        <li>Genre:</li>
+                                        <li>action, </li>
+                                        <li>Adventure, </li>
+                                        <li>Comedy, </li>
+                                        <li>Drama, </li>
+                                        <li>Fantasy, </li>
+                                        <li>Mature, </li>
+                                        <li>Romance, </li>
+                                        <li>Seinen, </li>
+                                    </ul>
+                                    <div class="star-rating">
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">star_half</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- 😒 -->
+                            <div class="created-on">
+                                <p>4/11/2024</p>
+                            </div>
+                            <div class="last-update">
+                                <p>3/21/2024</p>
+                            </div>
+                        </div>
+
+                        <div class="box-container">
+                            <div class="box">
+                                <div class="rank_image"><a href="../../img/books/domestic-na-kanojo.jpg "><img src="../../img/books/domestic-na-kanojo.jpg" alt=""></a>
+                                </div>
+                                <div class="chapter_contents">
+                                    <a class="Tfont" class="title" href="../mangapage/mangapage-domestic.php">Domestic Na Kanojo</a>
+                                    <ul>
+                                        <!--  GENRES-->
+                                        <li>Genre:</li>
+                                        <li>Drama, </li>
+                                        <li>Harem, </li>
+                                        <li>Mature, </li>
+                                        <li>Romance, </li>
+                                        <li>School life, </li>
+                                        <li>Shounen,</li>
+                                    </ul>
+                                    <div class="star-rating">
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">star_half</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- 😒 -->
+                            <div class="created-on">
+                                <p>4/11/2024</p>
+                            </div>
+                            <div class="last-update">
+                                <p>3/21/2024</p>
+                            </div>
+                        </div>
+
+                        <div class="box-container">
+                            <div class="box">
+                                <div class="rank_image"><a href="../../img/books/komisan.jpg "><img src="../../img/books/komisan.jpg" alt=""></a>
+                                </div>
+                                <div class="chapter_contents">
+                                    <a class="Tfont" class="title" href="../mangapage/mangapage-komisan.php">Komi-san wa Komyushou Desu</a>
+                                    <ul>
+                                        <!--  GENRES-->
+                                        <li>Genre:</li>
+                                        <li>Comedy, </li>
+                                        <li>Romance, </li>
+                                        <li>School, </li>
+                                        <li>life, </li>
+                                    </ul>
+                                    <div class="star-rating">
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">star_half</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- 😒 -->
+                            <div class="created-on">
+                                <p>4/11/2024</p>
+                            </div>
+                            <div class="last-update">
+                                <p>3/21/2024</p>
+                            </div>
+                        </div>
+
+                        <div class="box-container">
+                            <div class="box">
+                                <div class="rank_image"><a href="../../img/books/kingdom.jpg "><img src="../../img/books/kingdom.jpg" alt=""></a>
+                                </div>
+                                <div class="chapter_contents">
+                                    <a class="Tfont" class="title" href="../mangapage/mangapage-kingdom.php">Kingdom</a>
+                                    <ul>
+                                        <!--  GENRES-->
+                                        <li>Genre:</li>
+                                        <li>action, </li>
+                                        <li>Adventure, </li>
+                                        <li>Comedy, </li>
+                                        <li>Drama, </li>
+                                        <li>Historical, </li>
+                                        <li>Martial arts, </li>
+                                        <li>Romance, </li>
+                                    </ul>
+                                    <div class="star-rating">
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">grade</span>
+                                        <span class="material-symbols-outlined">star_half</span>
+                                    </div>
+
+                                </div>
+                            </div>
+                            <!-- 😒 -->
+                            <div class="created-on">
+                                <p>4/11/2024</p>
+                            </div>
+                            <div class="last-update">
+                                <p>3/21/2024</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>
+            </header>
+        </div>
+    </main>
+
+    <footer class="publish-footer"> <?php include('../php/footer.php'); ?> </footer>
+</body>
+
+</html>
