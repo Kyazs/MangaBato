06a87db46c29c35d752ba6352e6adf3d34fb6bbd
Kyazs <santosjohncasper@gmail.com>
2024-04-21 18:55:07 +0800

    view all added

---
+++ assets/viewall.php
@@ -0,0 +1,235 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MANGABATO</title>
+
+    <base href="../">
+
+    <!-- link for the star icon -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,1,0" />
+    <!-- link for the boxicons -->
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link rel="stylesheet" href="css/footer.css">
+    <link rel="stylesheet" href="css/maincontent.css">
+    <link rel="stylesheet" href="lib/mediaquery.css">
+    <link rel="stylesheet" href="/css/chapter.css">
+    <link rel="stylesheet" href="/css/viewall.css">
+    <link rel="icon" href="img/logo_mangabato.png" type="image/-icon">
+</head>
+
+<body>
+    <div class="container">
+        <nav id="navbar"></nav>
+        <main>
+            <div class="viewAll-container">
+                <article>
+                    <article class="latest_manga">
+                        <div class="topbar">
+                            <h2>Manga List</h2>
+                            <!-- <a href="#">View All</a> -->
+                        </div>
+                        <div class="latest_manga_content">
+                            <div class="latest_box">
+                                <a href="img/books/berserk.jpg"><img src="img/books/berserk.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="assets/mangapage/mangapage.php">BERSERK</a>
+                                    <ul>
+                                        <li>
+                                            <a href="/assets/chapters/chapter.html">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="/assets/chapters/chapter.html">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="/assets/chapters/chapter.html">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/gate.jpg"><img src="img/books/gate.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="assets/mangapage/mangapage-Gate.php">Gate -
+                                        Jietai
+                                        Kare No Chi Nite, Kaku Tatakeri</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/domestic-na-kanojo.jpg"><img src="img/books/domestic-na-kanojo.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="#">Domestic Na Kanojo</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/komisan.jpg"><img src="img/books/komisan.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="assets/mangapage/mangapage-komisan.php">Komi-san wa
+                                        Komyushou Desu</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/kingdom.jpg"><img src="img/books/kingdom.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="assets/mangapage/mangapage-Kingdom.php">Kingdom</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/berserk.jpg"><img src="img/books/berserk.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="#">BERSERK</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/berserk.jpg"><img src="img/books/berserk.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="#">BERSERK</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/berserk.jpg"><img src="img/books/berserk.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="#">BERSERK</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/berserk.jpg"><img src="img/books/berserk.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="#">BERSERK
+                                        kjsadnjasndajksdkjasndkasjdknasdnasdnaskj
+                                    </a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="latest_box">
+                                <a href="img/books/berserk.jpg"><img src="img/books/berserk.jpg" alt=""></a>
+                                <div class="title-chapter">
+                                    <a class="latest_content_title" href="#">BERSERK</a>
+                                    <ul>
+                                        <li>
+                                            <a href="#">Chapter 3</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 2</a>
+                                        </li>
+                                        <li>
+                                            <a href="#">Chapter 1</a>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <footer><button>Back</button><button>Next</button></footer>
+                        <div class="countpage"> <ul>
+                            <li>1</li>
+                            <li>2</li>
+                            <li>3</li>
+                            <li>4</li>
+                        </ul></div>
+                    </article>
+                </article>
+                <section>
+                    <?php include('../assets/php/trending.php'); ?>
+                </section>
+            </div>
+        </main>
+        <footer id="footer"></footer>
+    </div>
+
+    <script src="/js/navbar.js"></script>
+    <script src="/js/navfot.js" type="module"></script>
+</body>
+
+</html>
