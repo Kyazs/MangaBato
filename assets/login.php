61bb6ea5505734edbd90cd5dd8db82da34f7e551
Kyazs <santosjohncasper@gmail.com>
2024-04-30 20:04:16 +0800

    fixed login

--- assets/login.php
+++ assets/login.php
@@ -26,14 +26,25 @@
         <main>
             <div class="form_container">
                 <div class="login_container">
-                    <form action="/logon.html" method="post" enctype="multipart/form-data">
+                    <form action="/assets/php/authentication/user_auth.php" method="post" enctype="multipart/form-data">
                         <p>LOGIN</p>
                         <label for="username">Username:</label>
                         <input type="text" name="username" id="username" placeholder="Input username" required>
                         <label for="username">Password:</label>
                         <input type="password" name="password" id="password" placeholder="Input password" required>
+                        <label><i id="fgot" onclick="forgot()">Forgot Password? </i></label>
+                        <input type="submit" class="button" value="Login">
+                        <div id="error" style="color: red; font-size:15px;">
+                            <?php
+                            session_start();
+                            if (isset($_SESSION['error_message'])) {
+                                echo $_SESSION['error_message'];
+                                unset($_SESSION['error_message']); // Clear the error message from session
+                            }
+                            ?>
+                        </div><br>
                         <label id="signup"> Dont have an account? <i onclick="create()">Sign up</i></label>
-                        <button>Login</button>
+
                     </form>
                 </div>
                 <div class="signup_container">
@@ -48,9 +59,20 @@
                         <label for="profile">Upload Profile picture:</label>
                         <input type="file" accept="image/*" name="profile" id="profile" required>
                         <label for="biography">Description</label>
-                        <textarea name="biography" id="biography" cols="30" rows="10" style="resize: none;"></textarea>
-                        <input onclick="alerts()" type="submit" value="Create Account" id="submit">
+                        <textarea name="biography" id="biography" cols="30" rows="10"></textarea>
+                        <input onclick="alerts()" type="submit" value="Create Account" class="button"><br>
                         <label id="signup">Have an account? <i onclick="login()"> Login</i></label>
+                    </form>
+                </div>
+                <div class="forgot_container">
+                    <form id="forgot" action="" method="POST">
+                        <p>FORGOT PASSWORD</p>
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" placeholder="Enter Username" required>
+                        <label for="email">Email:</label>
+                        <input type="email" name="email" placeholder="Enter Email" required>
+                        <input onclick="alerts()" class="button" type="submit" value="Recover"><br>
+                        <label id="signup">Have an Account? <i onclick="login()">Login</i></label>
                     </form>
                 </div>
             </div>
@@ -71,12 +93,26 @@
             login.style.display = 'none'
             const signup = document.querySelector('.signup_container')
             signup.style.display = 'block'
+            const forgot = document.querySelector('.forgot_container')
+            forgot.style.display = 'none'
         }
+
         function login() {
             const login = document.querySelector('.login_container')
             login.style.display = 'block'
             const signup = document.querySelector('.signup_container')
             signup.style.display = 'none'
+            const forgot = document.querySelector('.forgot_container')
+            forgot.style.display = 'none'
+        }
+
+        function forgot() {
+            const login = document.querySelector('.login_container')
+            login.style.display = 'none'
+            const signup = document.querySelector('.signup_container')
+            signup.style.display = 'none'
+            const forgot = document.querySelector('.forgot_container')
+            forgot.style.display = 'block'
         }
     </script>
 </body>
